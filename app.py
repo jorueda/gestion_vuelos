24299b2e7a8396e2f69dace7aa8e4e154e95c64d
Randall Avila <dravila@uninorte.edu.co>
2021-10-25 14:02:05 -0500

    Mejora agregar, editar, eliminar vuelos

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ def login():
 	except:
 		return render_template('login.html', titulo="Sky Planner")
 
-
 @app.route('/registro/',methods=["GET", "POST"])
 def registro():
 	if request.method == 'POST':
@@ -88,13 +87,39 @@ def agregar_vuelo():
 
 @app.route('/agregado/', methods=['GET', 'POST'])
 def vuelo_agregado():
-	if request.method == 'POST':
-		id_vuelo = request.form['id_vuelo']
-		ciudad_origen = request.form['ciudad_origen']
-		ciudad_destino = request.form['ciudad_destino']
-		mensaje = "agregado"
-		return render_template('agregado.html', titulo="Agregar Vuelo", id_vuelo=id_vuelo, 
+	try:
+		if request.method == 'POST':
+			error = None
+			id_vuelo = request.form['id_vuelo']
+			ciudad_origen = request.form['ciudad_origen']
+			ciudad_destino = request.form['ciudad_destino']
+
+			if not id_vuelo:
+				error = 'Debes ingresar el id del vuelo'
+				flash(error)
+				return render_template( 'agregar.html' )
+
+			if not ciudad_origen:
+				error = 'Debes ingresar la ciudad de origen'
+				flash(error)
+				return render_template( 'agregar.html' )
+
+			if not ciudad_destino:
+				error = 'Debes ingresar la ciudad de destino'
+				flash(error)
+				return render_template( 'agregar.html' )
+			
+			db = get_db()
+			cur = db.cursor()
+			cur.executescript("INSERT INTO vuelo (id_vuelo, origen, destino) VALUES ('%s', '%s', '%s')" % (int(id_vuelo), ciudad_origen, ciudad_destino,))
+			db.commit()
+
+			mensaje = "Vuelo agregado con exito"
+			return render_template('agregado.html', titulo="Agregar Vuelo", id_vuelo=id_vuelo, 
 			ciudad_origen=ciudad_origen, ciudad_destino=ciudad_destino, mensaje=mensaje)
+	except:
+		flash('Error al crear vuelo')
+		return render_template('agregar.html', titulo="Agregar Vuelo")
 
 @app.route('/editar/', )
 def editar_vuelo():
@@ -102,15 +127,50 @@ def editar_vuelo():
 	
 @app.route('/editado/', methods=['GET', 'POST'])
 def vuelo_editado():
-	if request.method == 'POST':
-		id_vuelo = request.form['id_vuelo']
-		nombre = request.form['nombre']
-		apellido = request.form['apellido']
-		ciudad_origen = request.form['ciudad_origen']
-		ciudad_destino = request.form['ciudad_destino']
-		mensaje = "editado"
-		return render_template('actualizado.html', titulo="Editar Vuelo", id_vuelo=id_vuelo, nombre=nombre, 
-			apellido=apellido, ciudad_origen=ciudad_origen, ciudad_destino=ciudad_destino, mensaje=mensaje)
+	try:
+		if request.method == 'POST':
+			error = None
+
+			id_vuelo = request.form['id_vuelo']
+			ciudad_origen = request.form['ciudad_origen']
+			ciudad_destino = request.form['ciudad_destino']
+
+			if not id_vuelo:
+				error = 'Debes ingresar el id del vuelo'
+				flash(error)
+				return render_template( 'editar.html' , titulo="Editar Vuelo")
+
+			if not ciudad_origen:
+				error = 'Debes ingresar la ciudad de origen del vuelo'
+				flash(error)
+				return render_template( 'editar.html' , titulo="Editar Vuelo")
+
+			if not ciudad_destino:
+				error = 'Debes ingresar la ciudad de destino del vuelo'
+				flash(error)
+				return render_template( 'editar.html' , titulo="Editar Vuelo")
+
+			id_vuelo = int(id_vuelo)
+
+			db = get_db()
+			#cursor = db.execute('SELECT * FROM vuelo WHERE id_vuelo = ?', (id_vuelo,)).fetchone()
+			#cursor = db.execute('SELECT * FROM vuelo').fetchall()
+			cursor = db.execute('SELECT * FROM vuelo WHERE id_vuelo = ?', (id_vuelo,)).fetchall()
+
+			if(len(cursor) == 0):
+				error = 'Id de vuelo no encontrado'
+				flash(error)
+			else:
+				db.execute('UPDATE vuelo SET origen = ?, destino = ? WHERE id_vuelo=?', (ciudad_origen, ciudad_destino, id_vuelo,))
+				db.commit()
+
+			mensaje = "Vuelo editado correctamente"
+
+			return render_template('actualizado.html', titulo="Editar Vuelo", id_vuelo=id_vuelo, 
+			ciudad_origen=ciudad_origen, ciudad_destino=ciudad_destino, mensaje=mensaje)
+	except:
+		flash('Error al editar vuelo')
+		return render_template('editar.html', titulo="Editar Vuelo")
 
 @app.route('/eliminar/', )
 def eliminar_vuelo():
@@ -118,10 +178,37 @@ def eliminar_vuelo():
 
 @app.route('/eliminado/', methods=['GET', 'POST'])
 def vuelo_eliminado():
-	if request.method == 'POST':
-		id_vuelo = request.form['id_vuelo']
-		mensaje = "eliminado"
-		return render_template('actualizado.html', titulo="Eliminar Vuelo", id_vuelo=id_vuelo, mensaje=mensaje)
+	try:
+		if request.method == 'POST':
+			error = None
+
+			id_vuelo = request.form['id_vuelo']
+
+			if not id_vuelo:
+				error = 'Debes ingresar el id del vuelo'
+				flash(error)
+				return render_template( 'eliminar.html' , titulo="Eliminar Vuelo")
+
+			id_vuelo = int(id_vuelo)
+
+			db = get_db()
+			#cursor = db.execute('SELECT * FROM vuelo WHERE id_vuelo = ?', (id_vuelo,)).fetchone()
+			#cursor = db.execute('SELECT * FROM vuelo').fetchall()
+			cursor = db.execute('SELECT * FROM vuelo WHERE id_vuelo = ?', (id_vuelo,)).fetchall()
+
+			if(len(cursor) == 0):
+				error = 'Id de vuelo no encontrado'
+				flash(error)
+				return render_template( 'eliminar.html' , titulo="Eliminar Vuelo")
+			else:
+				db.execute('DELETE FROM vuelo WHERE id_vuelo=?', (id_vuelo,))
+				db.commit()
+
+			mensaje = "Vuelo eliminado correctamente"
+			return render_template('actualizado.html', titulo="Eliminar Vuelo", id_vuelo=id_vuelo, mensaje=mensaje)
+	except:
+		flash('Error al eliminar vuelo')
+		return render_template( 'eliminar.html' , titulo="Eliminar Vuelo")
 
 @app.route('/logout', )
 def logout():
